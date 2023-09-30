1139d9fecd4433c7eb8a68066065d56430ebafb9
Shaun Lamont <63021161+shaunl666@users.noreply.github.com>
2023-09-30 10:51:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -667,8 +667,8 @@ async function main() {
 	const url = new URL(
 		// "nike.splat",
 		// location.href,
-		params.get("url") || "train.splat",
-		"https://huggingface.co/cakewalk/splat-data/resolve/main/",
+		params.get("url") || "patrick.splat",
+		"https://huggingface.co/shaunl-666/splat/resolve/main/",
 	);
 	const req = await fetch(url, {
 		mode: "cors", // no-cors, *cors, same-origin
